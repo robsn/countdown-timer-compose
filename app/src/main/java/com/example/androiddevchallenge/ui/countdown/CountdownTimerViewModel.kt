c5cb6b2c7a17cbb49714c26599e50c8fe0c9a253
Robin Vinzenz <995962+robsn@users.noreply.github.com>
2021-03-04 16:52:01 +0100

    Satisfy spotless check =)

--- app/src/main/java/com/example/androiddevchallenge/ui/countdown/CountdownTimerViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/countdown/CountdownTimerViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.countdown
 
 import android.os.CountDownTimer
@@ -7,7 +22,7 @@ import kotlinx.coroutines.flow.StateFlow
 
 class CountdownTimerViewModel : ViewModel() {
 
-    private val startTimeInMillis = 1000L
+    private val startTimeInMillis = 10000L
 
     private val timeLeftInMillis = MutableStateFlow(startTimeInMillis)
 
@@ -36,7 +51,7 @@ class CountdownTimerViewModel : ViewModel() {
 
     private fun reset() {
         countDownTimer.cancel()
-        timeLeftInMillis.value = startTimeInMillis;
+        timeLeftInMillis.value = startTimeInMillis
         countDownTimer = freshCountdownTimer()
         _state.value = CountdownTimerViewState(startTimeInMillis, TimerState.Init)
     }
